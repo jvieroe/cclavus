cf6ff5ac5263069179013c731c6e3134f59ee37f
jvieroe <jvieroe@gmail.com>
2021-11-22 18:21:18 +0100

    NAMESPACE

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,12 @@
 ## code to prepare `DATASET` dataset goes here
+library(magrittr)
+
+url_baselayer <- "https://api.dataforsyningen.dk/regioner?format=geojson"
+baselayer <- sf::read_sf(url_baselayer)
+
+baselayer <- baselayer %>%
+  sf::st_transform(crs = 4326) %>%
+  sf::st_union()
+  dplyr::select(geometry)
 
 usethis::use_data(DATASET, overwrite = TRUE)

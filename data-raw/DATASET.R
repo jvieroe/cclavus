5e90daa7f8274329a97bfea9e7ce4b87b85b82ab
jvieroe <jvieroe@gmail.com>
2021-11-22 18:25:34 +0100

    baselayer data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -7,6 +7,5 @@ baselayer <- sf::read_sf(url_baselayer)
 baselayer <- baselayer %>%
   sf::st_transform(crs = 4326) %>%
   sf::st_union()
-  dplyr::select(geometry)
 
-usethis::use_data(DATASET, overwrite = TRUE)
+usethis::use_data(baselayer, overwrite = TRUE)

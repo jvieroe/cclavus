3a07d9a43a98295656af8459315f1c975cfe0b4a
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-23 15:32:28 +0100

    getBaseLayer update

--- R/cc_getBaseLayer.R
+++ R/cc_getBaseLayer.R
@@ -1,15 +1,26 @@
 #' Get lorem ipsum
 #'
 #' lorem ipsum
+#' @param as_sf A logical value. Return data as an object of class `sf`? Default is TRUE. If FALSE, imports as a GEOJSON of class `geomultipolygon`
 #' @return data lorem ipsum
 #' @author Jeppe Vierø
 #' @import sf dplyr
 #' @export
 
-cc_getBaseLayer <- function() {
+cc_getBaseLayer <- function(as_sf = TRUE) {
 
   baselayer <- cclavus::baselayer
 
-  return(baselayer)
+  if (af_sf == TRUE) {
+
+    base_layer <- geojsonsf::geojson_sf(baselayer)
+
+  } else if (sf_sf == FALSE) {
+
+    base_layer <- baselayer
+
+  }
+
+  return(base_layer)
 
 }
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,8 +1,4 @@
 ## code to prepare `DATASET` dataset goes here
-library(magrittr)
-library(sf)
-library(geojsonio)
-library(geojsonsf)
 
 baselayer <- sf::read_sf("https://api.dataforsyningen.dk/regioner?format=geojson")
 baselayer <- sf::st_union(baselayer)

45426075f79db9f6442128d82f4e0a0f601cfcbb
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-23 17:47:48 +0100

    Update DATASET.R

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,8 @@
 ## code to prepare `DATASET` dataset goes here
 
 baselayer <- sf::read_sf("https://api.dataforsyningen.dk/regioner?format=geojson")
+baselayer <- sf::st_transform(baselayer,
+                              crs = 4326)
 baselayer <- sf::st_union(baselayer)
 baselayer <- geojsonio::geojson_json(baselayer)
 

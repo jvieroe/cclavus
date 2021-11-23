6e982cc4701aa2034c684090763054144c8e9f87
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-23 17:47:12 +0100

    Update cc_getBaseLayer.R

--- R/cc_getBaseLayer.R
+++ R/cc_getBaseLayer.R
@@ -9,6 +9,8 @@
 cc_getBaseLayer <- function() {
 
   base_layer <- sf::read_sf("https://api.dataforsyningen.dk/regioner?format=geojson")
+  base_layer <- sf::st_transform(base_layer,
+                                 crs = 4326)
   base_layer <- sf::st_union(base_layer)
 
   return(base_layer)

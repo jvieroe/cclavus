15d3d83a0535855baa4725df38ce6df6336ae0b9
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-23 15:06:54 +0100

    temp data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,8 +1,15 @@
 ## code to prepare `DATASET` dataset goes here
 library(magrittr)
 library(sf)
+library(geojsonio)
 
 baselayer <- sf::read_sf("https://api.dataforsyningen.dk/regioner?format=geojson")
 baselayer <- sf::st_union(baselayer)
 
-usethis::use_data(baselayer, overwrite = TRUE, compress = "xz")
+object.size(baselayer)/1000000
+
+temp <- geojsonio::geojson_json(baselayer)
+
+object.size(temp)/1000000
+
+usethis::use_data(temp, overwrite = TRUE, compress = "xz")

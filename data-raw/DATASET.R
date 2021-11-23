1f0b4fb1d52c640f37dcfb42935aabd086dce27d
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-23 15:12:45 +0100

    Update DATASET.R

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -6,10 +6,4 @@ library(geojsonio)
 baselayer <- sf::read_sf("https://api.dataforsyningen.dk/regioner?format=geojson")
 baselayer <- sf::st_union(baselayer)
 
-object.size(baselayer)/1000000
-
-temp <- geojsonio::geojson_json(baselayer)
-
-object.size(temp)/1000000
-
-usethis::use_data(temp, overwrite = TRUE, compress = "xz")
+usethis::use_data(baselayer, overwrite = TRUE, compress = "xz")

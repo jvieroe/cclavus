48fe39512d69be1f966058f3a81ba187fed4c11c
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-23 17:46:22 +0100

    getBaselayer

---
+++ R/cc_getBaseLayer.R
@@ -0,0 +1,16 @@
+#' Get lorem ipsum
+#'
+#' lorem ipsum
+#' @return data lorem ipsum
+#' @author Jeppe Vierø
+#' @import sf dplyr
+#' @export
+
+cc_getBaseLayer <- function() {
+
+  base_layer <- sf::read_sf("https://api.dataforsyningen.dk/regioner?format=geojson")
+  base_layer <- sf::st_union(base_layer)
+
+  return(base_layer)
+
+}

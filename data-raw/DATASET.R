7bf6518be0113eca213deac487c987bd199830d4
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-23 15:28:52 +0100

    baselayer data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -6,16 +6,6 @@ library(geojsonsf)
 
 baselayer <- sf::read_sf("https://api.dataforsyningen.dk/regioner?format=geojson")
 baselayer <- sf::st_union(baselayer)
+baselayer <- geojsonio::geojson_json(baselayer)
 
-temp <- geojsonio::geojson_json(baselayer)
-
-
-sff <- geojson_sf(temp)
-
-object.size(sff)/1000000
-
-
-usethis::use_data(temp, overwrite = TRUE, compress = "xz")
-
-
-df <- cclavus::temp
+usethis::use_data(baselayer, overwrite = TRUE, compress = "xz")

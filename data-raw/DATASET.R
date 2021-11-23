df650b195bf70e7937fc3cec493fcb047ac86c9a
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-23 15:24:55 +0100

    add temp

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,8 +2,20 @@
 library(magrittr)
 library(sf)
 library(geojsonio)
+library(geojsonsf)
 
 baselayer <- sf::read_sf("https://api.dataforsyningen.dk/regioner?format=geojson")
 baselayer <- sf::st_union(baselayer)
 
-usethis::use_data(baselayer, overwrite = TRUE, compress = "xz")
+temp <- geojsonio::geojson_json(baselayer)
+
+
+sff <- geojson_sf(temp)
+
+object.size(sff)/1000000
+
+
+usethis::use_data(temp, overwrite = TRUE, compress = "xz")
+
+
+df <- cclavus::temp
